786630f41dfbe0441ecb43f9c5af04501466c97d
SamHoeterickx <samhoeterickx111@gmail.com>
2026-06-08 12:29:05 +0200

    feat: init styles

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -1,6 +1,14 @@
 import { Outlet } from "@tanstack/react-router";
 import type { FC, PropsWithChildren } from "react";
+import cxBind from "classnames/bind";
+
+import styles from "./app.module.scss"
+const cx = cxBind.bind(styles)
 
 export const App: FC<PropsWithChildren> = () => {
-    return <Outlet />
+    return (
+        <div className={cx('cApp')}>
+            <Outlet />
+        </div>
+    )
 }
--- src/modules/home/page/Home.page.tsx
+++ src/modules/home/page/Home.page.tsx
@@ -1,14 +1,17 @@
 import type { FC } from "react";
 import { useTranslation } from "react-i18next";
+import cxBind from "classnames/bind";
 
-import 
+// STYLES
+import styles from './home.module.scss';
+const cx = cxBind.bind(styles);
 
 export const HomePage: FC = () => {
 
     const { t } = useTranslation();
 
     return (
-        <div>
+        <div className={cx('cHome')}>
             <h1>{t('home.title')}</h1>
         </div>
     )
